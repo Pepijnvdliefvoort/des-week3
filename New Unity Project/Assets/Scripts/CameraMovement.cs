4163533fce7eb432d1a7384a60d6e3cc6f69e5ec
Pepijn <pepijnvdliefvoort@live.nl>
2021-09-23 11:19:06 +0200

    Added movement and ability to pick up keys

---
+++ New Unity Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CameraMovement : MonoBehaviour {
+    Vector2 input;
+    List<GameObject> inventory;
+
+    [SerializeField]
+    public int Speed = 5;
+
+    // Start is called before the first frame update
+    void Start() {
+        inventory = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        input = context.ReadValue<Vector2>();
+    }
+
+    public void OnClick(InputAction.CallbackContext context) {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.tag == "Clickable") {
+                inventory.Add(hit.transform.gameObject);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+
+    public void Move() {
+        Vector3 Xaxis = input.x * transform.right;
+        Vector3 Yaxis = input.y * transform.forward;
+
+        transform.Rotate(new Vector3(0, input.x / 2, 0));
+
+        Vector3 direction = (Xaxis + Yaxis).normalized * Time.deltaTime * Speed;
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (!Physics.Raycast(ray, 1)) {
+            transform.position += direction;
+        }
+    }
+}
